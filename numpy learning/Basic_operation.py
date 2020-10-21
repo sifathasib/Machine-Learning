0411eabfbb7c854a8c7d84c61d4563449dd9f8d7
sifat <sifathasib@gmail.com>
2020-10-21 11:26:53 +0600

    first commit

---
+++ numpy learning/Basic_operation.py
@@ -0,0 +1,57 @@
+import numpy as np 
+
+"""a = np.array([10,20,30,40])
+b = np.array([0,1,2,3])
+ 
+print(a-b)
+print(b**2)
+print(a<35)"""
+
+"""A =  np.array([
+    [1,1],
+    [0,1]
+])
+B = np.array([
+    [2,0],
+    [3,4]
+])
+
+print(A*B)
+print(A @ B)
+print(A.dot(B))"""
+
+'''rg  = np.random.default_rng(10)
+a = np.ones((2,3),dtype=int)
+b = rg.random((2,3))
+c =  np.empty((2,3))
+a*=3
+b += a
+print(a)
+print(b)
+print(c)'''
+
+'''b = np.linspace(11,50,3)
+print(b)'''      
+
+
+'''a = np.arange(6).reshape((2,3))
+b = np.array([
+    [1,3,4],
+    [1,2,3]])
+ 
+print(a)
+print(b)
+
+print(b.sum(axis=0))
+print(b.sum(axis=1))
+print(b.min(axis=0))
+
+print(np.concatenate([a,b],axis=1))
+print(np.concatenate([a,b],axis=0))'''
+
+
+
+
+
+
+
---
+++ numpy learning/test.py
@@ -0,0 +1,7 @@
+import numpy as np 
+a = np.arange(6).reshape(2,3)
+b = np.arange(0,48,2).reshape(2,3,4)
+c = np.arange(10000).reshape(100,100)
+print(c)
+print(a)
+print(b)
